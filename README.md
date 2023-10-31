5eda78f7a9082bda2c626776a62c8d84d7a248d8
Aaron Cox <aaron@greymass.com>
2023-10-31 16:49:14 -0700

    Initial boilerplate

---
+++ README.md
@@ -0,0 +1,3 @@
+# Svelte + TS + Vite + Pico.css
+
+A basic boilerplate based on `npm create vite@latest` with the sample app stripped out, and pico.css added in.
---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+</script>
+
+<header>
+    <hgroup>
+        <h1>App</h1>
+        <h1>Empty project for Svelte + Vite + Pico.css</h1>
+    </hgroup>
+</header>
+
+<style>
+</style>
