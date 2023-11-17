1e186e28171ab8bfdd3b15ff660bd0160645cc4d
Aaron Cox <aaron@greymass.com>
2023-11-17 11:19:27 -0800

    Implement session controls in UI

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,31 @@
 <script lang="ts">
+    import { onMount } from 'svelte'
+    import { login, logout, restore, session } from './wharf'
+
+    onMount(restore)
 </script>
 
 <header>
-    <hgroup>
-        <h1>App</h1>
-        <h1>Empty project for Svelte + Vite + Pico.css</h1>
-    </hgroup>
+    <div class="grid">
+        <div>
+            <hgroup>
+                <h1>TODO!</h1>
+                <h1>A Todo app using for Wharf + Svelte + Vite + Pico.css</h1>
+            </hgroup>
+        </div>
+        <div>
+            {#if $session}
+                <button on:click={logout}>Logout ({$session.actor})</button>
+            {:else}
+                <button on:click={login}>Login</button>
+            {/if}
+        </div>
+    </div>
 </header>
 
 <style>
+    .grid button {
+        width: auto;
+        float: right;
+    }
 </style>
